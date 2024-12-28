7a77ea9ff8789848c3c416762ae05708f2c95d4b
Ekichi <102156984+gorbodoh@users.noreply.github.com>
2024-12-28 10:59:15 +0800

    Initial commit.

---
+++ JsonLoader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JsonLoaderPackage;
+
+namespace JsonLoader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean isJsonLoaderOk = false;
+
+            // Standard hello world.
+            Console.WriteLine("Hello World!");
+
+            isJsonLoaderOk = JsonLoaderService.LoadJson("TEST!");
+            if (isJsonLoaderOk)
+            {
+                Console.WriteLine("Everything is fine!");
+            }
+            else
+            {
+                Console.WriteLine("Not fine! Not fine!");
+            }
+
+            return;
+        }
+    }
+}
---
+++ JsonLoaderPackage/JsonLoaderService.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonLoaderPackage;
+
+public class JsonLoaderService
+{
+    public static Boolean LoadJson(string filePath)
+    {
+        Console.WriteLine($"JsonLoaderService.LoadJson() invoked! You have parsed in \"{filePath}\"!");
+
+        return true;
+    }
+}
